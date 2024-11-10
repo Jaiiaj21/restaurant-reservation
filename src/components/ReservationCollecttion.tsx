c857197b3255d8087b0bbdb3e71303170a290458
SupanutT <supanut.tangsin@gmail.com>
2024-11-10 16:26:33 +0700

    feat: update reservation

--- src/components/ReservationCollecttion.tsx
+++ src/components/ReservationCollecttion.tsx
@@ -13,7 +13,7 @@ const ReservationCollection = async ({ reservationJson, role, user_id }: { reser
           restaurantJsonReady.data.map((reservationItem: ReservationItem) => {
             if (role === "admin" || (role === "user" && user_id === reservationItem.user)) {
               return (
-                <div key={reservationItem._id} className="bg-slate-200 rounded p-5 w-[80%] my-3">
+                <div key={reservationItem._id} className="bg-slate-200 rounded p-5 w-[80%] my-3 relative">
                   <div className="text-xl">
                     User: {reservationItem.user}
                   </div>
@@ -29,9 +29,18 @@ const ReservationCollection = async ({ reservationJson, role, user_id }: { reser
                   <div className="text-xl">
                     Number of Guest: {reservationItem.numOfGuests}
                   </div>
-                  <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm mt-5">
-                    Remove from Cart
-                  </button>
+                  <div className="absolute right-2 bottom-2 flex items-center">
+                    <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm m-3">
+                      <Link href={`/reservations/update/${reservationItem._id}?name=${reservationItem.restaurant?.name}`}>
+                        Update reservation
+                      </Link>
+                    </button>
+                    <button className="block rounded-md bg-red-600 hover:bg-red-400 px-3 py-2 text-white shadow-sm m-3"
+
+                    >
+                      Delete restaurant
+                    </button>
+                  </div>
                 </div>
               )
             }
