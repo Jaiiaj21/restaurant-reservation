0f927d9802b2dfc524337ec1006a8994fa5b62fd
Jaiiaj21 <yossawat.ongudomluk@gmail.com>
2024-11-20 16:08:12 +0700

    feat: expose env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["drive.google.com"],
   },
+  experimental: {
+    serverActions: true,
+  },
+  env: {
+    NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+  },
 };
 
 export default nextConfig;
